912baf351942b1d4a066159c7ca1dbefffe56c9a
beni <53890189+benisdp@users.noreply.github.com>
2023-09-14 16:35:12 -0400

    change home page redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/import"
+              href="/dashboard"
             >
               <h3 className="text-2xl font-bold">Get Started</h3>
               <div className="text-lg">
