02a26e1d9478c1e0e7adf437031ed72f19b00966
Ali Eskandari <AliEskandari3@gmail.com>
2023-09-15 10:14:40 -0700

    added file to deploy weaviate on kubernetes

--- scripts/scryfall/import-cards-into-db.ts
+++ scripts/scryfall/import-cards-into-db.ts
@@ -10,81 +10,94 @@ const DATA_PATH = "./test/data/scryfall/cards-with-base64.json";
 
 let count = 0;
 (async () => {
-  // Delete class if it already exists
-  await client.schema.classDeleter().withClassName("ScryfallCard").do();
+  try {
+    console.log("Deleting class...");
 
-  await client.schema
-    .classCreator()
-    .withClass({
-      class: "ScryfallCard",
-      moduleConfig: {
-        "img2vec-neural": {
-          imageFields: ["image"],
-        },
-      },
-      properties: [
-        {
-          dataType: ["blob"],
-          description: "Grayscale image",
-          name: "image",
-        },
-        {
-          dataType: ["text"],
-          description: "card name",
-          name: "name",
-          tokenization: "word",
-        },
-      ],
-      vectorizer: "img2vec-neural",
-      vectorIndexConfig: {
-        distance: "cosine",
-      },
-    })
-    .do();
+    // Delete class if it already exists
+    const resp = await client.schema
+      .classDeleter()
+      .withClassName("ScryfallCard")
+      .do();
 
-  let batcher = client.batch.objectsBatcher();
+    console.dir(resp, { depth: 1 });
 
-  const pipeline = chain([
-    fs.createReadStream(DATA_PATH),
-    parser(),
-    streamArray(),
-    async ({ key, value: card }: { key: number; value: Card }) => {
-      try {
-        // add card to import batch
-        console.log(`Adding card number ${key}...`, card.name);
-        if (!card.base64 || !card.name) {
-          console.log("No base64 image found");
-          return;
-        }
-        count++;
+    console.log("Creating class...");
 
-        if (count % 100 === 0) {
-          await batcher.withConsistencyLevel("ALL").do();
-          console.log(`${count} cards imported!`);
-          batcher = client.batch.objectsBatcher();
-        }
-
-        batcher = batcher.withObject({
-          properties: {
-            name: card.name,
-            image: card.base64,
+    await client.schema
+      .classCreator()
+      .withClass({
+        class: "ScryfallCard",
+        moduleConfig: {
+          "img2vec-neural": {
+            imageFields: ["image"],
           },
-          class: "ScryfallCard",
-        });
-      } catch (error) {
-        console.log("Error importing card", card.name, error);
-      }
-    },
-  ]);
+        },
+        properties: [
+          {
+            dataType: ["blob"],
+            description: "Grayscale image",
+            name: "image",
+          },
+          {
+            dataType: ["text"],
+            description: "card name",
+            name: "name",
+            tokenization: "word",
+          },
+        ],
+        vectorizer: "img2vec-neural",
+        vectorIndexConfig: {
+          distance: "cosine",
+        },
+      })
+      .do();
 
-  pipeline.on("finish", async () => {
-    try {
-      console.log("Importing cards...");
-      const resp = await batcher.withConsistencyLevel("ALL").do();
-      console.dir(resp, { depth: 1 });
-      console.log(`${count} cards imported!`);
-    } catch (error) {
-      console.log("Error importing cards", error);
-    }
-  });
+    let batcher = client.batch.objectsBatcher();
+
+    const pipeline = chain([
+      fs.createReadStream(DATA_PATH),
+      parser(),
+      streamArray(),
+      async ({ key, value: card }: { key: number; value: Card }) => {
+        try {
+          // add card to import batch
+          console.log(`Adding card number ${key}...`, card.name);
+          if (!card.base64 || !card.name) {
+            console.log("No base64 image found");
+            return;
+          }
+          count++;
+
+          if (count % 100 === 0) {
+            await batcher.withConsistencyLevel("ALL").do();
+            console.log(`${count} cards imported!`);
+            batcher = client.batch.objectsBatcher();
+          }
+
+          batcher = batcher.withObject({
+            properties: {
+              name: card.name,
+              image: card.base64,
+            },
+            class: "ScryfallCard",
+          });
+        } catch (error) {
+          console.log("Error importing card", card.name, error);
+        }
+      },
+    ]);
+
+    pipeline.on("finish", async () => {
+      try {
+        console.log("Importing cards...");
+        const resp = await batcher.withConsistencyLevel("ALL").do();
+        console.dir(resp, { depth: 1 });
+        console.log(`${count} cards imported!`);
+      } catch (error) {
+        console.log("Error importing cards", error);
+      }
+    });
+  } catch (error) {
+    console.log("Error importing cards", error);
+  }
 })();
--- weaviate/client/index.ts
+++ weaviate/client/index.ts
@@ -1,10 +1,13 @@
-import weaviate, { WeaviateClient, ApiKey } from "weaviate-ts-client";
+import weaviate from "weaviate-ts-client";
+
+const WEAVIATE__HOST = "34.172.254.77";
+const OPENAI__API_KEY = "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";
 
 export const client = weaviate.client({
   scheme: "http",
-  host: "localhost:8080", // Replace with your endpoint
+  host: WEAVIATE__HOST, // Replace with your endpoint
   // apiKey: new ApiKey("rvIOODTLIJaMropyiudJQiSk2nOZjaNzMM21"),
   headers: {
-    "X-OpenAI-Api-Key": "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",
+    "X-OpenAI-Api-Key": OPENAI__API_KEY,
   },
 });
